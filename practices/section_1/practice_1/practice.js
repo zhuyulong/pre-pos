358eb46f91e6361936bf2744eefb6223853a8acf
zhuyulong <356981944@qq.com>
2015-05-17 17:04:11 +0800

    add first test

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,16 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var collection_same=[];
+for(var i = 0; i <collection_a.length; i++){
+    for(var j = 0 ;j<collection_b.length; j++){
+        if(collection_a[i]==collection_b[j]){
+            collection_same.push(collection_a[i]);
+        }
+    }
+
 }
+return collection_same;
+}
+
 
 module.exports = collect_same_elements;
