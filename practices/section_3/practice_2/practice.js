ca0129c40aa7b3b87682c44c4a4b221fbbe6d4e0
zhuyulong <356981944@qq.com>
2015-05-17 22:39:16 +0800

    add sever pre-pos

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,5 +1,15 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
-}
+  for(var i=0;i<collection_a.length;i++){
 
+         for(var k=0;k<object_b.value.length;k++){
+           var sum=0;
+              if(collection_a[i].key==object_b.value[k]){
+                        sum=  parseInt(collection_a[i].count/3);
+                       collection_a[i].count= collection_a[i].count-sum ;
+              }
+       }
+    }
+    return collection_a;
+  }
 module.exports = create_updated_collection;
