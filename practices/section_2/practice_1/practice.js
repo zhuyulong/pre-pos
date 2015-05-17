4e1dc7db813087162373a472ed348d92aa9140b7
zhuyulong <356981944@qq.com>
2015-05-17 22:27:00 +0800

    add five pre-pos

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,23 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var array = [];
+  var flog = false;  // 定义标志
+
+  for(var i = 0; i < collection.length; i++){
+    for(var j = 0; j < array.length; j++){
+        if(array[j].key == collection[i]){  // 判断是否找到
+          array[j].count ++;  // 找到count++
+          flog = true;
+        }
+    }
+    if(!flog){   // 找不到，创建
+        var temp = {};
+        temp.key = collection[i];
+        temp.count = 1;
+        array.push(temp);
+      }
+      flog = false;
+    }
+    return array;
 }
 
 module.exports = count_same_elements;
